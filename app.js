a574a0a9d72adb52c98d07a023d3d892fadf23ba
Ruslan Te <ruslan.te.alt@gmail.com>
2025-06-29 17:42:04 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3001
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}...`)
+});
+
