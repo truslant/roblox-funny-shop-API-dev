34e555561dd5ebad6f1196f08f24d66fed2455c1
Ruslan Te <ruslan.te.alt@gmail.com>
2025-07-02 22:12:57 +0300

    revised associations - change from object to array based approach

--- SQLs/schema.sql
+++ SQLs/schema.sql
@@ -1,44 +1,50 @@
-DROP TABLE IF EXISTS users, products, carts, carts_products;
+DROP TABLE IF EXISTS users,
+products,
+carts,
+carts_products;
 
 CREATE TABLE
     users (
         id SERIAL PRIMARY KEY,
-        firstName text,
-        lastName text,
-        email text UNIQUE
+        firstName text NOT NULL,
+        lastName text NOT NULL,
+        email text UNIQUE NOT NULL
     );
 
 CREATE TABLE
     products (
         id SERIAL PRIMARY KEY,
-        name text,
-        description text,
-        price numeric(10, 2)
+        name text NOT NULL,
+        description text NOT NULL,
+        price numeric(10, 2) NOT NULL
     );
 
-CREATE TABLE carts (
-    id SERIAL PRIMARY KEY,
-    userId integer REFERENCES users(id) UNIQUE,
-);
+CREATE TABLE
+    carts (
+        id SERIAL PRIMARY KEY,
+        userId integer REFERENCES users (id) UNIQUE ON DELETE CASCADE,
+    );
 
-CREATE TABLE carts_products (
-    id SERIAL PRIMARY KEY,
-    cartid integer REFERENCES carts(id),
-    productid integer REFERENCES products(id),
-    qty integer NOT NULL,
-    UNIQUE (cartId, productid) 
-);
+CREATE TABLE
+    carts_products (
+        id SERIAL PRIMARY KEY,
+        cartid integer REFERENCES carts (id) ON DELETE CASCADE,
+        productid integer REFERENCES products (id) ON DELETE CASCADE,
+        qty integer NOT NULL,
+        UNIQUE (cartId, productid)
+    );
 
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    userid integer REFERENCES users(id)
-);
+CREATE TABLE
+    orders (
+        id SERIAL PRIMARY KEY,
+        userid integer REFERENCES users (id) ON DELETE CASCADE
+    );
 
-CREATE TABLE orders_products (
-    id SERIAL PRIMARY KEY,
-    orderid integer REFERENCES orders(id),
-    productid integer REFERENCES products(id),
-    qty integer,
-    UNIQUE (orderid, productid)
-);
-
+CREATE TABLE
+    orders_products (
+        id SERIAL PRIMARY KEY,
+        orderid integer REFERENCES orders (id) ON DELETE CASCADE,
+        productid integer REFERENCES products (id) ON DELETE CASCADE,
+        qty integer,
+        UNIQUE (orderid, productid)
+    );
--- db/database.js
+++ db/database.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize').Sequelize;
+
 const getUserModel = require('./models/UserModel');
 const getCartModel = require('./models/CartModel');
+const getProductModel = require('./models/ProductModel');
+const getCartsProductsModel = require('./models/CartsProductsModel');
+const getOrdersProductsModel = require('./models/OrdersProductsModel');
 
 const db = process.env.DB;
 const username = process.env.USER_NAME;
@@ -16,62 +20,119 @@ const sequelize = new Sequelize(
     }
 );
 
-const User = getUserModel(sequelize, Sequelize);
-const Cart = getCartModel(sequelize, Sequelize);
+const models = {}
 
-const models = { User, Cart }
+models.User = getUserModel(sequelize, Sequelize);
+models.Cart = getCartModel(sequelize, Sequelize);
+models.Product = getProductModel(sequelize, Sequelize);
+models.CartsProducts = getCartsProductsModel(sequelize, Sequelize);
+models.OrdersProducts = getOrdersProductsModel(sequelize, Sequelize);
 
-const associations = {
-    User: {
-        hasOne: 'Cart',
+
+const associations = [
+    {
+        sourceModelName: 'User',
+        targetModelName: 'Cart',
+        association: 'hasOne',
         options: { onDelete: 'CASCADE' }
     },
-    Cart: {
-        belongsTo: 'User',
-        options: { onDelete: 'CASCADE', foreignKey: 'userid' }
-    }
-}
-
-Object.keys(models).forEach(sourceModelName => {
-
-    if (associations[sourceModelName]) {
-
-        const targetModelName = associations[sourceModelName].hasOne
-            ||
-            associations[sourceModelName].hasMany
-            ||
-            associations[sourceModelName].belongsTo
-            ||
-            associations[sourceModelName].belongsToMany
-
-        const sourceModel = models[sourceModelName];
-        const targetModel = models[targetModelName];
-
-        if (associations[sourceModelName].hasOne) {
-            sourceModel.hasOne(
-                targetModel,
-                associations[sourceModelName].options
-            );
+    {
+        sourceModelName: 'Cart',
+        targetModelName: 'User',
+        association: 'belongsTo',
+        options: {
+            foreignKey: 'userid',
+            onDelete: 'CASCADE',
         }
-        if (associations[sourceModelName].hasMany) {
-            sourceModel.hasMany(
-                targetModel,
-                associations[sourceModelName].options
-            );
+    },
+    {
+        sourceModelName: 'Cart',
+        targetModelName: 'Product',
+        association: 'belongsToMany',
+        options: {
+            through: 'CartsProducts',
+            foreignKey: 'cartid',
+            otherKey: 'productid',
+            onDelete: 'CASCADE'
         }
-        if (associations[sourceModelName].belongsTo) {
-            sourceModel.belongsTo(
-                targetModel,
-                associations[sourceModelName].options
-            )
+    },
+    {
+        sourceModelName: 'Product',
+        targetModelName: 'Cart',
+        association: 'belongsToMany',
+        options: {
+            through: 'CartsProducts',
+            foreignKey: 'productid',
+            otherKey: 'cartid',
+            onDelete: 'CASCADE'
         }
-        if (associations[sourceModelName].belongsToMany) {
-            sourceModel.belongsToMany(
-                targetModel,
-                associations[sourceModelName].options
-            )
+    },
+    {
+        sourceModelName: 'User',
+        targetModelName: 'Order',
+        association: 'hasMany',
+        options: { onDelete: 'CASCADE' }
+    },
+    {
+        sourceModelName: 'Order',
+        targetModelName: 'User',
+        association: 'belongsTo',
+        options: {
+            foreignKey: 'userid',
+            onDelete: 'CASCADE'
+        }
+    },
+    {
+        sourceModelName: 'Order',
+        targetModelName: 'Product',
+        association: 'belongsToMany',
+        options: {
+            through: 'OrdersProducts',
+            foreignKey: 'orderid',
+            otherKey: 'productid',
+            onDelete: 'CASCADE'
+        }
+    },
+    {
+        sourceModelName: 'Product',
+        targetModelName: 'Order',
+        association: 'belongsToMany',
+        options: {
+            through: 'OrdersProducts',
+            foreignKey: 'productid',
+            otherKey: 'orderid',
+            onDelete: 'CASCADE'
+        }
+    },
+]
+
+associations.forEach(associationInstance => {
+    const {
+        sourceModelName,
+        targetModelName,
+        association,
+        options } = associationInstance;
+
+    const sourceModel = models[sourceModelName];
+    const targetModel = models[targetModelName];
+
+    if (sourceModel) {
+        switch (association) {
+            case 'hasOne':
+                sourceModel.hasOne(targetModel, options);
+                break;
+            case 'belongsTo':
+                sourceModel.belongsTo(targetModel, options);
+                break;
+            case 'hasMany':
+                sourceModel.hasMany(targetModel, options);
+                break;
+            case 'belongsToMany':
+                sourceModel.belongsToMany(targetModel, options);
+                break;
         }
     }
 });
 
 module.exports = { sequelize, models }
+
